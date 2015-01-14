ad4b7da4162f640a12bd66af39c5dc4b34e3534f
Barbakas <jjcorps@gmail.com>
2015-01-15 00:27:41 +0100

    Tarea 1. Terminada

--- sql/devs/npc_tarea1.sql
+++ sql/devs/npc_tarea1.sql
@@ -1,9 +1,38 @@
 SET @Entry :=50001;
 SET @ModelID :=27436;
-SET @Name :='Izan';
-SET @Subname :='Tremblay';
+SET @Name :='Izan Tremblay';
+SET @Subname :='';
 SET @NPCFLAG :=1; -- 1 is gossip - http://collab.kpsn.org/display/tc/creature_template#creature_template-npcflag
 SET @ScriptName :='npc_tarea1';
 DELETE FROM `creature_template` WHERE `entry`=@Entry;
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction_A`, `faction_H`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `mindmg`, `maxdmg`, `dmgschool`, `attackpower`, `dmg_multiplier`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `minrangedmg`, `maxrangedmg`, `rangedattackpower`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `spell5`, `spell6`, `spell7`, `spell8`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `HoverHeight`, `Health_mod`, `Mana_mod`, `Armor_mod`, `RacialLeader`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`, `WDBVerified`) VALUES
 (@Entry, 0, 0, 0, 0, 0, @ModelID, 0, 0, 0, @Name, @Subname, '', 0, 80, 80, 2, 35, 35, @NPCFLAG, 1, 1.14286, 1, 3, 10, 10, 0, 0, 1, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1, 10000, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, @ScriptName, 1);
+
+-- type texts: Say(12) Yell(14) Emote(16) Whisper(15) Boss Emote(41) Boss Whisper(42)
+DELETE FROM `creature_text` WHERE `entry`=@Entry;
+INSERT INTO `creature_text`(`entry`,`groupid`,`id`,`text`,`type`,`comment`)
+VALUES (@Entry,0,0,'You\'re like an angel',15,CONCAT(@Name,' - SAY_ANGEL_TRANSFORM'));
+
+INSERT INTO `creature_text`(`entry`,`groupid`,`id`,`text`,`type`,`comment`)
+VALUES (@Entry,1,0,'Nobody spits in my face!',14,CONCAT(@Name,' - SAY_SPIT'));
+
+
+INSERT INTO `creature_text`(`entry`,`groupid`,`id`,`text`,`type`,`comment`)
+VALUES (@Entry,2,0,'You will be reduced to ashes',14,CONCAT(@Name,' - SAY_YELL_1'));
+
+INSERT INTO `creature_text`(`entry`,`groupid`,`id`,`text`,`type`,`comment`)
+VALUES (@Entry,3,0,'I will tear your face off',14,CONCAT(@Name,' - SAY_YELL_2'));
+
+
+INSERT INTO `creature_text`(`entry`,`groupid`,`id`,`text`,`type`,`comment`)
+VALUES (@Entry,4,0,'That\'s all you can do?',14,CONCAT(@Name,' - SAY_YELL_3'));
+
+INSERT INTO `creature_text`(`entry`,`groupid`,`id`,`text`,`type`,`comment`)
+VALUES (@Entry,5,0,'I will rip off you apart',14,CONCAT(@Name,' - SAY_YELL_4'));
+
+INSERT INTO `creature_text`(`entry`,`groupid`,`id`,`text`,`type`,`comment`)
+VALUES (@Entry,6,0,'As you wish',12,CONCAT(@Name,' - SAY_AGGRO'));
+
+INSERT INTO `creature_text`(`entry`,`groupid`,`id`,`text`,`type`,`comment`)
+VALUES (@Entry,7,0,'This ends now!',14,CONCAT(@Name,' - SAY_BERSERK'));
+
--- src/server/scripts/Custom/npc_tarea1.cpp
+++ src/server/scripts/Custom/npc_tarea1.cpp
@@ -1,8 +1,12 @@
 /*
 npc_tarea1.cpp
 
-Blah Blah Blah
+Bien, tu primera tarea. Deberás hacer en código un npc custom que tenga un gossip (menú).
 
+- Cuando hagas click en el para abrirlo, te transforme en cerdo
+- Debe tener dos opciones. Una de ellas, para ponerte en combate e iniciar una batalla con al menos 4 spell y 2 diálogos. La segunda opción, debe transportarte a Dalaran
+- Cuando le lances un beso, te transformará en Angel
+- Cuando le escupas, deberá matarte.
 
 By Barbakas
 */
@@ -12,46 +16,190 @@ By Barbakas
 #include "ScriptedGossip.h"
 
 
+enum ScriptTexts //IDs in the creature_text Table
+{
+	//Neutral
+	SAY_ANGEL_TRANSFORM = 0,	
+	SAY_SPIT = 1,
+
+	//Combat
+	SAY_YELL_1 = 2,
+	SAY_YELL_2 = 3,
+	SAY_YELL_3 = 4,
+	SAY_YELL_4 = 5,
+	SAY_AGGRO = 6,
+	SAY_BERSERK = 7,
+	SAY_DEATH = 8,
+	SAY_KILL = 9,
+};
+
+enum Spells
+{	
+	SPELL_PUMMEL		= 12555,
+	SPELL_POISON_BOLT	= 24099,
+	SPELL_UPPERCUT		= 39069,
+	SPELL_KNOCKBACK		= 26027,
+	//SPELL_FRENZY		= 23537,
+	SPELL_BERSERK		= 32965,
+};
+
+enum Events
+{
+	EVENT_YELL = 1,
+	EVENT_DAMAGE = 2,
+	EVENT_KNOCKING = 3,
+	EVENT_DOT = 4,
+	EVENT_BERSERK = 5
+};
+
+enum Models
+{
+	MODEL_PIG				= 16358,
+	MODEL_ANGEL				= 5233,
+};
+
+enum eEnums
+{
+	// any other constants
+	FACTION_WORGEN = 24
+};
+
+//List of gossip item texts. Items will appear in the gossip window.
+#define GOSSIP_FIGHT		"I'm looking for a fight"
+#define GOSSIP_DALARAN		"Could you teleport me to Dalaran?"
+
 class npc_tarea1 : public CreatureScript
 {
 public:
 	npc_tarea1() :CreatureScript("npc_tarea1") {}
 
-	bool OnGossipHello(Player* player, Creature* creature)
+	struct Tarea1AI : public ScriptedAI
 	{
-		if (player->isInCombat())
+		Tarea1AI(Creature* creature) : ScriptedAI(creature) {}		
+
+		void Reset()
 		{
-			player->GetSession()->SendNotification("You are in combat!");
-			return false;
+			me->RestoreFaction();
 		}
 
-		player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Combate", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
-		player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Angel", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
+		void EnterCombat(Unit* who)
+		{
+			//Prepare the events
+			events.Reset();
+			events.ScheduleEvent(EVENT_YELL, 10000);
+			events.ScheduleEvent(EVENT_DAMAGE, 5000);
+			events.ScheduleEvent(EVENT_DOT, urand(10000, 20000));
+			events.ScheduleEvent(EVENT_KNOCKING, 19000);
+			events.ScheduleEvent(EVENT_BERSERK, 120000);
+
+			//Say some stuff			
+			Talk(SAY_AGGRO);
+		}
+
+		void UpdateAI(uint32 const diff)
+		{
+			if (!UpdateVictim() )
+				return;
+
+			events.Update(diff);
+
+			if (me->HasUnitState(UNIT_STATE_CASTING))
+				return;
+
+			while (uint32 eventId = events.ExecuteEvent())
+			{
+				switch (eventId)
+				{
+				case EVENT_YELL:
+					Talk(RAND(SAY_YELL_1, SAY_YELL_2, SAY_YELL_3, SAY_YELL_4));
+					events.ScheduleEvent(EVENT_YELL, 20000);
+					break;
+				case EVENT_DAMAGE:
+					DoCastVictim(SPELL_PUMMEL);
+					events.ScheduleEvent(EVENT_DAMAGE, 5000);
+					break;
+
+				case EVENT_DOT:
+					DoCastVictim(SPELL_POISON_BOLT);
+					events.ScheduleEvent(EVENT_DOT, urand(10000, 20000));
+					break;
+
+				case EVENT_KNOCKING:
+					DoCastVictim(RAND(SPELL_UPPERCUT, SPELL_KNOCKBACK));
+					events.ScheduleEvent(EVENT_KNOCKING, 19000);
+					break;
+
+				case EVENT_BERSERK:
+					DoCast(me, SPELL_BERSERK);
+					Talk(SAY_BERSERK);
+					break;
+
+				}
+
+			}
+			
+			DoMeleeAttackIfReady();
+		
+		}
+
+		void ReceiveEmote(Player* player, uint32 uiTextEmote)
+		{
+			me->HandleEmoteCommand(uiTextEmote);
+
+			switch (uiTextEmote)
+			{
+			case TEXT_EMOTE_KISS:
+				Talk(SAY_ANGEL_TRANSFORM);
+				player->SetDisplayId(MODEL_ANGEL);
+				break;
+			case TEXT_EMOTE_SPIT:
+				if (player->isGossip()) player->PlayerTalkClass->ClearMenus();
+				Talk(SAY_SPIT);
+				me->DealDamage(player, 1000000);//kill (ugly way...)
+				break;
+			}
+		}
+
+	protected:
+		EventMap events;
+
+	};
+
+	CreatureAI* GetAI(Creature* creature) const
+	{
+		return new Tarea1AI(creature);
+	}
+
+	bool OnGossipHello(Player* player, Creature* creature)
+	{
+
+		player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_FIGHT, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+		player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_DALARAN, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
 		
 		player->PlayerTalkClass->SendGossipMenu(907, creature->GetGUID());
 
-		player->SetDisplayId(16358);
+		//Transformacion en cerdo
+		player->SetDisplayId(MODEL_PIG);
 
 		return true;
 	}
 
 	bool OnGossipSelect(Player* player, Creature* creature, uint32 /*sender*/, uint32 actions)
 	{
-		//if (!player) return false;
-
 		player->PlayerTalkClass->ClearMenus();
 
-		if (actions == GOSSIP_ACTION_INFO_DEF + 1)
+		if (actions == GOSSIP_ACTION_INFO_DEF + 1) //Fight
 		{
-			//player->SetDisplayId(16358);
-			creature->MonsterSay("¡¡¡LUCHAAAA!!!",LANG_UNIVERSAL,0);
 			player->CLOSE_GOSSIP_MENU();
+
+			creature->setFaction(FACTION_WORGEN);
+			creature->AI()->AttackStart(player);
+
 		}
-		else if (actions == GOSSIP_ACTION_INFO_DEF + 2)
+		else if (actions == GOSSIP_ACTION_INFO_DEF + 2)//Dalaran
 		{
-			player->SetDisplayId(25900);
-			creature->MonsterSay("Angelito...", LANG_UNIVERSAL, 0);
 			player->CLOSE_GOSSIP_MENU();
+			player->TeleportTo(571, 5804.585f, 624.726f, 647.780f, 1.820647f);
 		}
 
 		return true;
